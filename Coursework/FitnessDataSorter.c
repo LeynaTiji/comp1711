46f1147af9ddc0b0bc8e55c6f6d2fecad29799e0
LeynaTiji <146225194+LeynaTiji@users.noreply.github.com>
2023-12-01 11:36:23 +0000

    add

--- Coursework/FitnessDataSorter.c
+++ Coursework/FitnessDataSorter.c
@@ -14,16 +14,16 @@ typedef struct {
 FitnessData arrayofdata[200];
 
 // Function to tokenize a record
-void tokeniseRecord(char *record, char *delimiter, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, delimiter);
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
     if (ptr != NULL) {
         strcpy(date, ptr);
-        ptr = strtok(NULL, delimiter);
+        ptr = strtok(NULL, &delimiter);
         if (ptr != NULL) {
             strcpy(time, ptr);
-            ptr = strtok(NULL, delimiter);
+            ptr = strtok(NULL, &delimiter);
             if (ptr != NULL) {
-                steps = atoi(ptr);
+                *steps = atoi(ptr);
             }
         }
     }
